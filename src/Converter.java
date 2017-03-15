c952c7d00e6cb80570999665f16f6035b5b349fa
Cpearce95 <christopherpearce10@gmail.com>
2017-03-15 01:09:43 +0000

    Add files via upload
    Added method to convert Hashmap strings to their double values. 
    Added a way of retrieving amount from user.

--- src/Converter.java
+++ src/Converter.java
@@ -20,19 +20,22 @@ public class Converter {
 	
 	public static void main(String[] args) {
 		
-		Path p = FileSystems.getDefault().getPath("Currencies" ,"currencies.txt");
-		currency c = new currency();
-		HashMap<String, Double> currencies = c.readCurrenciesToMap(p);
+		Path p = FileSystems.getDefault().getPath("Currencies" ,"currencies.txt"); //This is the path to the flat file with exchange rates stored
+		currency c = new currency();	//This a new currency object. Used for accessing methods 
+		HashMap<String, Double> currencies = c.readCurrenciesToMap(p); //Create new map and assign to value of returned Hashmap
 		
-		String currencyFrom;
-		String currencyTo;
-		String name;
+		String currencyFrom; //This a string variable representing the currency user chooses to exchange from
+		String currencyTo;	//This a string variable representing the currency user chooses to exchange to
+		String Surname; 	//This variable stores the name of the user
+		double amountFrom;	//This is the amount that user wishes to exchange	
+		double amountTo;	//This is the amount that user will receive
 		Scanner input = new Scanner(System.in);
-		//ask for other details
+		
 		
 	
 		
-		
+		System.out.println("Please enter your Surname?");
+		Surname = input.nextLine();
 		
 		
 		do{
@@ -48,11 +51,22 @@ public class Converter {
 		
 		
 		
-		System.out.println("How much " + currencyFrom + " Would you like to convert?");
-		//ask for how much
-		//make sure amount is greater than 0
-		//do calculation 
+		System.out.println("How much " + currencyFrom + " would you like to convert to " + currencyTo);
 		
+		
+		
+		do {
+			System.out.println("Please enter a value greater than 0");
+			amountFrom = input.nextDouble();
+		}while (amountFrom < 0);
+			
+		
+		System.out.println();
+		
+	
+		//get proper rates
+		//do calculation 
+		//do write to file
 		input.close();
 	}
 	
--- src/currency.java
+++ src/currency.java
@@ -60,6 +60,19 @@ public class currency {
 		return false;
 	}
 	
+	public double getConversionRate (HashMap<String, Double> currencyMap, String currency){
+		
+		for(String s : currencyMap.keySet()){
+			
+			if(s.equals(currency)){
+				return currencyMap.get(s);
+			}
+			
+		}
+		
+		return 0;
+	}
+	
 	
 	
 }
